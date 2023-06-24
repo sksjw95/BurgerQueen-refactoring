b7c81d120b1e0994640598fd0bd6228e0da6da59
sksjw95 <tjwldnd95@hanmail.net>
2023-06-24 20:03:54 +0900

    day10

---
+++ src/com/codestates/seb/burgerqueen/Order.java
@@ -0,0 +1,45 @@
+package com.codestates.seb.burgerqueen;
+
+import com.codestates.seb.burgerqueen.discount.discountCondition.CozDiscountCondition;
+import com.codestates.seb.burgerqueen.discount.discountCondition.KidDiscountCondition;
+import com.codestates.seb.burgerqueen.product.ProductRepository;
+
+public class Order {
+    private Cart cart;
+
+    public Order(Cart cart){
+        this.cart = cart;
+    }
+    public void makeOrder(){
+        // 여기부터
+        CozDiscountCondition cozDiscountCondition = new CozDiscountCondition();
+        KidDiscountCondition kidDiscountCondition = new KidDiscountCondition();
+
+        cozDiscountCondition.checkDiscountCondition();
+        kidDiscountCondition.checkDiscountCondition();
+        // 여기까지 추가
+
+        int totalPrice = cart.calculateTotalPrice();
+
+        // 여기부터
+        int finalPrice = totalPrice;
+        if (cozDiscountCondition.isSatisfied()){
+            finalPrice = cozDiscountCondition.applyDiscount(finalPrice);
+        }
+        if (kidDiscountCondition.isSatisfied()){
+            finalPrice = kidDiscountCondition.applyDiscount(finalPrice);
+        }
+        // 여기까지 추가
+        System.out.println("[📢] 주문이 완료되었습니다.");
+        System.out.println("[📢] 주문 내역은 다음과 같습니다.");
+        System.out.println("-".repeat(60));
+
+
+        // 상품 상세 내역 출력
+        cart.printCartItemDetails();
+
+        System.out.println("-".repeat(60));
+        System.out.printf("금액 합계    :%d원\n", totalPrice);
+        System.out.printf("할인 적용 금액  :%d원\n", finalPrice); // 추가
+    }
+}
